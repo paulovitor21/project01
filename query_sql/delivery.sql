4b7dda38904a102c002608a71e49c6be5037250c
paulovitor21 <paulovitorpereira21@outlook.com>
2024-12-08 11:05:20 -0400

    innital commit

---
+++ query_sql/delivery.sql
@@ -0,0 +1,6 @@
+SELECT * FROM public.table_delivery_status;
+
+SELECT * FROM public.table_delivery_status WHERE "Item" = 'MFZ66175402';
+
+SELECT * FROM table_delivery_status
+WHERE "ORG" = 'NW1';
---
+++ query_sql/pph.sql
@@ -0,0 +1,8 @@
+SELECT * FROM public.table_pph;
+
+SELECT * FROM public.table_pph
+WHERE "Org." = 'NWV';
+
+SELECT * FROM public.table_pph
+WHERE "Org." = 'NWV'
+AND "Date" = '2024-12-04 03:00:00';
